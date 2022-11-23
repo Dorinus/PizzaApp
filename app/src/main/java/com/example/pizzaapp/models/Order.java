5faba7dd86b43765dad03ad021971879818c6886
Dorin Chira <47989101+Dorinus@users.noreply.github.com>
2022-11-23 15:54:55 +0100

    Order

---
+++ app/src/main/java/com/example/pizzaapp/models/Order.java
@@ -0,0 +1,33 @@
+package com.example.pizzaapp.models;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class Order {
+    private ArrayList<Product> products;
+
+    public double getTotalPrice(){
+        double totalPrice = 0.0;
+        for(int i = 0; i < products.size(); i++){
+            totalPrice+= products.get(i).getPrice();
+        }
+        return totalPrice;
+    }
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public void removeProduc(Product product){
+        try {
+            products.remove(products.indexOf(product));
+        }catch (Exception e){
+            Log.e("NoProduct", "removeProduc: ",e );
+        }
+    }
+
+    public int totalItems(){
+        return products.size();
+    }
+}
